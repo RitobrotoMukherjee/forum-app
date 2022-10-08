57d4b7296746995b37397d60b4adc6bfe68e2abb
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-08 20:31:38 +0530

    Set-up env variable and home page

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 
 // Invoke App
 const app = express();
 
+app.get('/', (req, resp) => {
+    resp.send({ message: "Home Page" });
+});
+
 // Listen
-app.listen(3030, () => {
-    console.warn('Listenning to port 3030');
+app.listen(process.env.PORT, () => {
+    console.warn('Listenning to port ', process.env.PORT);
 });
