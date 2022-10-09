b8b8b9f9d8f6d91e98ea7a5729336a660dcc0474
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-09 15:14:49 +0530

    Add model and schema

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const ForumModel = require('../models/forum_model');
 
 const router = express.Router();
 
-router.get('/', (req, resp) => {
-    resp.send({ msg: "Get all list" });
+router.get('/', (_, resp) => {
+    ForumModel.find()
+    .then((data) => resp.status(200).send({ data, msg: "Get all list" }))
+    .catch(err => resp.status(400).send({ error: true, msg: err}));
 });
 
 router.post('/', (req, resp) => {
-    resp.send({ msg: "Create New Entry" });
+    const { title, description, type } = req.body;
+    const data = new ForumModel({ title, description, type });
+    const errors = data.validateSync();
+    if(errors) resp.status(400).send({ error: true, msg: errors});
+    else resp.status(200).send({ data, msg: "Data Created" })
 });
 
 router.delete('/:id', (req, resp) => {
